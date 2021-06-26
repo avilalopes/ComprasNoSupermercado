0adddca6d9cd6c53900969e78bef6ee855203d58
avilalopes <82402280+avilalopes@users.noreply.github.com>
2021-06-25 22:48:50 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+class MinhaClasse {
+  public static void Main (string[] args) {
+    
+    Console.WriteLine("Informe a quantidade de listas: ");
+    var totalDeListas = int.Parse(Console.ReadLine());
+    var n = totalDeListas;
+
+    string[] listaDeCompras;
+    SortedSet<string> lista;
+
+    for (int i = 0; i < n; i++)
+    {
+        Console.WriteLine();
+        Console.WriteLine("Informe os itens da lista: ");        
+        listaDeCompras = Console.ReadLine().Split(' ');
+        lista = new SortedSet<string>(listaDeCompras);
+
+        foreach (var item in lista)
+        {
+            Console.Write(item.Replace(" ",""));
+            Console.Write(' ');
+        }
+        lista.Clear();
+        Console.WriteLine();
+    }
+  }
+}
